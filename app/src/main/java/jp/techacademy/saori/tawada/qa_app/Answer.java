75c3473fcc3112547d786284836b46d50aa1a88b
Saori Tawada <cktistwd@gmail.com>
2018-03-14 03:04:21 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/saori/tawada/qa_app/Answer.java
@@ -0,0 +1,37 @@
+package jp.techacademy.saori.tawada.qa_app;
+
+/**
+ * Created by admin on 2018/03/13.
+ */
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+}
